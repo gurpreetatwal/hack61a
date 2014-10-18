4e9a3ecd89a64968f879b0137b8f87a96081a38c
unknown <harrison.hsueh@berkeley.edu>
2014-10-18 15:42:51 -0700

    countcourses

--- classes.py
+++ classes.py
@@ -31,6 +31,31 @@ class CourseLoad(Course):
 	def unit_combos_phase1():
 		units = 10.5
 
+	def count_courses(courses,amount=10.5):
+	    """Return the number of ways to make change for amount.
+
+	    >>> count_change(7)
+	    6
+	    >>> count_change(10)
+	    14
+	    >>> count_change(20)
+	    60
+	    >>> count_change(100)
+	    9828
+	    """
+	    def help_change(amount, part):
+	        if amount == 0:
+	            return 1
+	        if amount == part:
+	            return 1
+	        elif amount < part:
+	            return 0
+	        else:
+	            return help_change(amount-part.lst[1].units,part)+help_change(amount,part*2)
+	    return help_change(amount,courses.lst)
+	    possible_courses = course_load
+	    
+	    possible_courses.print()
 
 
 	def combo():
