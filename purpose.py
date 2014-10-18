ed1edc0ab65e882a382889779156f8c32fb4e71a
unknown <harrison.hsueh@berkeley.edu>
2014-10-18 14:24:41 -0700

    purpose file

---
+++ purpose.py
@@ -0,0 +1,14 @@
+Abilities:
+
+Percent and ‘current/total’ enrollment at phase1_day(self)
+
+Percent and ‘current/total’ enrollment at phase2_day(self)
+
+Percent and ‘current/total’ enrollment at end_phase2(self)
+
+User input: 
+classes
+
+
+
+
