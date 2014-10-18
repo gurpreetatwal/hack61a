a6fa5b7acf838981688ecf0bc26cdbf96369f206
unknown <harrison.hsueh@berkeley.edu>
2014-10-18 14:59:40 -0700

    update2 purposes

--- purpose.py
+++ purpose.py
@@ -1,10 +1,17 @@
-Abilities:
+Data:
 
 Percent and ‘current/total’ enrollment at phase1_day(self)
-
 Percent and ‘current/total’ enrollment at phase2_day(self)
-
 Percent and ‘current/total’ enrollment at end_phase2(self)
+
+Output:
+
+Data
+print("over capacity for", course,"by phase1")
+print("Possible combinations for phase 3", list_of_courses_in_order_from_fullest_to_emptiest_during_phase_1_less_than_10.5_units)
+print("Must at least phase2", list_of_courses)
+print("Must phase1", list_of_courses)
+
 
 User input: 
 classes
