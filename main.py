096156eb218fe2f052abb1d64be577e9eb351ced
Garret Lin <garretlin@airbears2-10-142-34-220.airbears2.1918.berkeley.edu>
2014-10-18 20:32:49 -0700

    everything

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ while i < len(user_classes):
 			waitlisted = day['waitlisted']
 			wait_percent = day['waitlisted_percent']
 			enro_percent = day['enrolled_percent']
-			class_data[current_class][day_key] = [enrolled, waitlisted, enro_percent, wait_percent, units, max_enroll]
+			class_data[current_class][day_key] = [enrolled, waitlisted, enro_percent, wait_percent, int(units), max_enroll]
 		i += 1
 	else:
 		user_classes[i] = input('Sorry, but ' + user_classes[i] + ' is not a valid course, please try again\n')
